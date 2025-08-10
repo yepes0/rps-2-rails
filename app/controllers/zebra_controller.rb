1d468caa40cb4ab6691a3cb9affd65ee5a62785a
yepes0 <71985093+yepes0@users.noreply.github.com>
2025-08-10 22:03:11 +0000

    listo papá

--- app/controllers/zebra_controller.rb
+++ app/controllers/zebra_controller.rb
@@ -1,16 +1,16 @@
 class ZebraController < ApplicationController
   def rock
-    @random_move = ["rock", "paper", "scissors"].sample
+    @comp_move = ["rock", "paper", "scissors"].sample
     render({ :template => "game_templates/play_rock" })
   end
 
   def paper
-    @random_move = ["rock", "paper", "scissors"].sample
+    @comp_move = ["rock", "paper", "scissors"].sample
     render({ :template => "game_templates/play_paper" })
   end
 
   def scissors
-    @random_move = ["rock", "paper", "scissors"].sample
+    @comp_move = ["rock", "paper", "scissors"].sample
     render({ :template => "game_templates/play_scissors" })
   end
 
